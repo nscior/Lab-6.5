12aea1bdd73f7ccef5df1383464b06b52cb3375f
nscior <82701815+nscior@users.noreply.github.com>
2024-03-02 17:28:27 -0500

    completed function

---
+++ names.js
@@ -0,0 +1,18 @@
+const adjectiveArr = ["Silly", "Cool", "Wild", "Young", "Angry", "Bored", "Loud", "Quiet", "Silent", "Dissonant"];
+
+const modeArr =["Major", "Minor", "Dorian", "Locrian", "Phygrian", "Mixolydian", "Aeolian", "Diminished", "Pentatonic", "Chromatic"];
+
+let joined = ''; // empty string
+
+// this should take in two arrays and spit out one string object
+function generate() {
+    // randInt = Math.floor(Math.random() * 10) + 1;
+
+    for (let i in adjectiveArr) {
+        for (let j in modeArr) {
+            joined = adjectiveArr[Math.floor(Math.random() * 10)] + ' ' + modeArr[Math.floor(Math.random() * 10)];
+        }
+    }
+
+    return alert(joined);
+}
