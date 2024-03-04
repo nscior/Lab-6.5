7bc40197f779f8c2a69331f4c4a00ce2115206d6
nscior <sciorti3@msu.edu>
2024-03-04 15:21:10 -0500

    update, header works, button not functional

--- names.js
+++ names.js
@@ -5,9 +5,8 @@
 // this should take in two arrays and spit out one string object
 function generate() {
     const adjectiveArr = ['Silly', 'Cool', 'Wild', 'Young', 'Angry', 'Bored', 'Loud', 'Quiet', 'Silent', 'Dissonant'];
-
     const modeArr =['Major', 'Minor', 'Dorian', 'Locrian', 'Phygrian', 'Mixolydian', 'Aeolian', 'Diminished', 'Pentatonic', 'Chromatic'];
-
+    
     for (let i in adjectiveArr) {
         for (let j in modeArr) {
             joined = adjectiveArr[Math.floor(Math.random() * 9) + 1] + ' ' + modeArr[Math.floor(Math.random() * 9) + 1];
